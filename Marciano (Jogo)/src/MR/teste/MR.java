ffa59072f159652d818cd405a0dfb2e72a4ea2fc
Miuke <156957779+Miuke0200@users.noreply.github.com>
2024-01-17 18:18:21 -0300

    Add files via upload

---
+++ Marciano (Jogo)/src/MR/teste/MR.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MR.teste;
+
+import Marciano.NewJFrame;
+
+/**
+ *
+ * @author Aluno
+ */
+public class MR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame Marciano  = new NewJFrame();
+        Marciano.setVisible(true);
+        
+    }
+    
+}
