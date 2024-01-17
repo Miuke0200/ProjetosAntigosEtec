4461b7e9a39e29d21aa83d4ceab464a544e0a5ca
Miuke <156957779+Miuke0200@users.noreply.github.com>
2024-01-17 18:14:29 -0300

    Add files via upload

---
+++ SUSetec/src/classes/conexao.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+package classes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+/**
+ *
+ * @author User
+ */
+public class conexao {
+    
+    public static Statement statement = null;
+
+    // String com o caminho onde está o banco de dados
+    String URL = "jdbc:mysql://carmine:3306/2dsa_miuke_etecsus";
+    //Login
+    String usuario = "aluno";
+    //Senha
+    String senha = "etec@147";
+    // Variavel para o comando SQL
+    private Statement stm = null;
+    // Variavel para a conexão
+    private Connection conexao = null;
+ 
+    // Métodos Conectar e Desconectar Banco de Dados 
+    // =============================================
+
+    public void conectar() throws ClassNotFoundException, SQLException {
+       
+        // Carga do driver de conexão
+        Class.forName("com.mysql.jdbc.Driver");
+        // Fazendo a conexão
+        conexao = DriverManager.getConnection(URL, usuario, senha);
+        statement = (Statement) conexao.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+    }
+
+    public void desconectar() throws SQLException{
+           
+        // Fechando a conexão
+        conexao.close();
+    }
+public static int runSQL(String sql) 
+    {
+        int qtdreg = 0;
+     
+    try{
+      qtdreg = statement.executeUpdate(sql);
+      //   JOptionPane.showMessageDialog(null, "Registro processado");
+      }catch(SQLException sqlex){
+           System.out.println("Erro acesso ao BD"+ sqlex);
+      //   JOptionPane.showMessageDialog(null, "Erro");
+      }
+   return qtdreg;
+    }
+
+
+}
+
